21c65ced2143049162a44932030356dfc7ee7591
lgou2w <lgou2w@hotmail.com>
2025-07-30 19:25:09 +0800

    feat: enhance app header with GitHub link and Netlify badge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 export default function App() {
   return (
     <div>
-      <h1>HoYo.Gacha</h1>
+      <h1><a href="https://github.com/lgou2w/HoYo.Gacha">HoYo.Gacha</a></h1>
       <p>Welcome to your new app.</p>
       <p>Happy coding!</p>
+      <br />
+      <p>Deploys by <a href="https://www.netlify.com">Netlify</a></p>
+      <a href="https://www.netlify.com">
+        <img src="https://www.netlify.com/assets/badges/netlify-badge-color-accent.svg" alt="Deploys by Netlify" />
+      </a>
     </div>
   )
 }
